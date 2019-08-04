6643c3d39ceb4cf4a40c5d8c19b72fae3f274be9
Divy123 <lit2017001@iiitl.ac.in>
2019-08-05 04:53:14 +0530

    Backend complete

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 let left_red_channel, right_red_channel,img;
 
+readyScript();
+
 function changeScreen(event) {
-  console.log(img)
   var op = event.target.className;
   if (op.includes("btn-next")) {
     $(event.target).parents(".screen").fadeOut("slow");
     $(event.target).parents(".screen").next().fadeIn("slow");
-    console.log($(event.target).parents(".screen").attr('class'))
+
     if($(event.target).parents(".screen").attr('class').includes("container-3")){
       document.getElementById('stepTwo').appendChild(img);
+      $(".container-4 .btn-next").attr("disabled", true);
       $('img#sel').imgAreaSelect({
         handles: true,
         autoHide:true,
@@ -23,31 +25,54 @@ function changeScreen(event) {
   } else if (op.includes("btn-back")) {
     $(event.target).parents(".screen").fadeOut("slow");
     $(event.target).parents(".screen").prev().fadeIn("slow");
+    if($(event.target).parents(".screen").attr('class').includes("container-5")){
+      document.getElementById('stepTwo').appendChild(img);
+      $('img#sel').imgAreaSelect({
+        handles: true,
+        autoHide:true,
+        onSelectEnd: cb
+      });
+      right_red_channel = undefined;
+      $(".container-4 .btn-next").attr("disabled", true);
+    }
+    else if($(event.target).parents(".screen").attr('class').includes("container-4")){
+      document.getElementById('gallery').appendChild(img);
+      $('img#sel').imgAreaSelect({
+        handles: true,
+        autoHide:true,
+        onSelectEnd: cb
+      });
+      left_red_channel = undefined;
+      $(".container-3 .btn-next").attr("disabled", true);
+    }
+    else if($(event.target).parents(".screen").attr('class').includes("container-3")){
+      left_red_channel = undefined;
+    right_red_channel = undefined;
+    readyScript()
+    }
   } else if (op.includes("btn-over")) {
     $(event.target).parents(".screen").fadeOut("slow");
     $(".container-2").fadeIn("slow");
+    left_red_channel = undefined;
+    right_red_channel = undefined;
+    readyScript();
   }
 }
 
-
-
-let dropArea = document.getElementById('drop-area');;
-['dragenter', 'dragover', 'dragleave', 'drop'].forEach(
+function readyScript(){
+  let dropArea = document.getElementById('drop-area');
+;['dragenter', 'dragover', 'dragleave', 'drop'].forEach(
   eventName => {
     dropArea.addEventListener(eventName, preventDefaults, false)
   })
-
 function preventDefaults(e) {
   e.preventDefault()
   e.stopPropagation()
 }
-
 ;
 ['dragenter', 'dragover'].forEach(eventName => {
   dropArea.addEventListener(eventName, highlight, false)
 })
-
-
 ;
 ['dragleave', 'drop'].forEach(eventName => {
   dropArea.addEventListener(eventName, unhighlight, false)
@@ -66,40 +91,14 @@ dropArea.addEventListener('drop', handleDrop, false)
 function handleDrop(e) {
   let dt = e.dataTransfer
   let files = dt.files
-
   handleFiles(files)
 }
-
-function handleFiles(files) {
-  ([...files]).forEach(previewFile)
-}
-
-function previewFile(file) {
-  let reader = new FileReader()
-  reader.readAsDataURL(file)
-  reader.onloadend = function () {
-    img = document.createElement('img')
-    img.setAttribute("id", "sel");
-    img.src = reader.result
-    $("#drop-area").parents(".screen").fadeOut("slow");
-    $("#gallery").parents(".screen").fadeIn("slow", displayPreview);
-  }
-
-  function displayPreview() {
-    document.getElementById('gallery').appendChild(img);
-    $('img#sel').imgAreaSelect({
-      handles: true,
-      autoHide:true,
-      onSelectEnd: cb
-    });
-  }
 }
 
 function cb(img, selection) {
   var sequencer = imp.ImageSequencer({
     inBrowser: false
   })
-
   sequencer.loadImage(img.src, function () {
     this.addSteps('crop{x:' + selection.x1 + '|y:' + selection.y1 + '|w:' + selection.width + '|h:' + selection.height + '},average').run(function (out) {
       imp.getPixels(out, function (err, pixels) {
@@ -111,9 +110,7 @@ function cb(img, selection) {
           sum[2] += pixels.data[i++];
           sum[3] += pixels.data[i++];
         }
-
         let divisor = pixels.data.length / 4;
-
         sum[0] = Math.floor(sum[0] / divisor);
         if(!left_red_channel)
         {
@@ -124,11 +121,36 @@ function cb(img, selection) {
         right_red_channel = sum[0];
         $(".container-4 .btn-next").attr("disabled", false);
         }
-        console.log(left_red_channel+"    "+right_red_channel)
       })
     })
-
-
   })
+}
 
+function handleFiles(files) {
+  
+  ([...files]).forEach(previewFile)
+}
+
+function previewFile(file) {
+  $("input")[0].value='';
+  let reader = new FileReader()
+  reader.readAsDataURL(file)
+  reader.onloadend = function () {
+    img = document.createElement('img')
+    img.setAttribute("id", "sel");
+    img.src = reader.result
+    $("#drop-area").parents(".screen").fadeOut("slow");
+    $("#gallery").parents(".screen").fadeIn("slow", displayPreview);
+    $(".container-3 .btn-next").attr("disabled", true);
+  }
+
+  function displayPreview() {
+    if($("#gallery img")) $("#gallery img").remove();
+    document.getElementById('gallery').appendChild(img);
+    $('img#sel').imgAreaSelect({
+      handles: true,
+      autoHide:true,
+      onSelectEnd: cb
+    });
+  }
 }
